e65fa8a6d9e5eb3b51fd2c414ab2af67d12d61f6
james947 <jmuriuki947@gmail.com>
2017-07-18 11:40:33 -0700

    add new branch

---
+++ flask_Bucket/app.py
@@ -0,0 +1,13 @@
+from flask import flask
+
+app = flask(__name__)
+
+@app.route('/')
+
+
+def index():
+    return  'hello world'
+
+if __name__= '__main__':
+
+app.run(debug='True')
---
+++ flask_Bucket/static/js/script.js
@@ -0,0 +1,10 @@
+//pops an input for adding a new list
+
+window.onload=function()
+{
+    var el=document.getElementById('button');
+    el.onclick=function(){
+        var my_text=prompt('Add new activity');
+
+    }
+}
