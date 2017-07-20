844f812ac78116f82cc0fffc8a9289283bba3c84
james947 <jmuriuki947@gmail.com>
2017-07-20 10:17:46 -0700

    add fuction to add activity

---
+++ flask_Bucket/bucky.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+app = Flask(__name__)
+
+app.secret_key= '\x9a\x9fc!\x00\x14\xbf\n\xd6\xfa\xbf\xaf\xb7\xdc]\x0c~\\\x01L\xbe:\x05W'
+bucket_activities=[]
+
+def store_activities(title, activity):
+    bucket_activities.append(title=title, activity=activity)
+
+
+@app.route('/')
+def index():
+    return 'Flask runnig success'
+
+#starts user session
+@app.route('/login', methods=['GET','POST'] )
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'user' and request.form['password'] != 'user':
+            error ='invalid credentials.please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('bucket'))
+    return  render_template("login.html", error = error)
+
+#creates a new post and renders to the same page
+@app.route('/bucket', methods=['GET','POST'])
+def bucket():
+    if request.method == 'POST':
+        title= request.form['title']
+        activity= request.form['activity']
+        store_activities(title, activity)
+        return redirect(url_for('bucket'))
+    return render_template("bucketlist.html", bucket_activities=(dict(bucket_activities)))
+
+#routes to the login page
+@app.route('/register')
+def register():
+    return  render_template("register.html")
+#Ends the session
+@app.route('/logout')
+def logout():
+    session.pop('logged in', None)
+    return redirect(url_for('login'))
+
+
+if __name__== '__main__':
+    app.run(debug='True')
+
